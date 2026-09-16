38401f38e6df7d8fe693ea9f461b26a2497842bb
Mohan-10-15 <mohanakrishnan1510@gmail.com>
2026-09-16 21:37:44 +0530

    Format vite.config.js for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/atti-verse-main/'
-  server: {
-    port: 5173,
-    open: true,
-    host: true,
-  }
-  build: {
-    target: 'es2018',
-    sourcemap: false,
-  }
+plugins: [react()],
+base: '/atti-verse-main/',
+server: {
+port: 5173,
+open: true,
+host: true,
+},
+build: {
+target: 'es2018',
+sourcemap: false,
+},
 })
-
