990803697e9f7f8c55944456fd05a110f4e55f45
Mohan-10-15 <mohanakrishnan1510@gmail.com>
2026-09-16 21:36:39 +0530

    Fix formatting issues in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/atti-verse-main/',
+  base: '/atti-verse-main/'
   server: {
     port: 5173,
     open: true,
     host: true,
-  },
+  }
   build: {
     target: 'es2018',
     sourcemap: false,
-  },
+  }
 })
+
